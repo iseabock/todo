c5174829611a1b7f6201456010fedfd653d2c528
Ian Seabock <ianseabock@gmail.com>
2020-12-08 19:04:33 -0800

    clearence gem added for auth

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ProjectsController < ApplicationController
+    before_action :require_login
     before_action :set_project, only: %i[show edit update destroy]
 
     # GET /projects
